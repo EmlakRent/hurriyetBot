b9592226abf6322db89fc80a8e741cb2d0b160af
erdemoflaz <erdemoflaz4@gmail.com>
2016-04-06 22:14:01 +0300

    resimler tamamlandı

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ preg_match_all('@<a id="ctl00_cphContent_ctlRealtyListNew1_rptRealtyList_lnkOver
 
 
 
-for ( $j = 0; $j < 50 ; $j++ )
+for ( $j = 0; $j < 20 ; $j++ )
 {
     # Current url
     $url ="http://www.hurriyetemlak.com".$detay_icin_link[2][$j]."<br>";
@@ -19,10 +19,17 @@ for ( $j = 0; $j < 50 ; $j++ )
     preg_match_all('@<meta itemprop="latitude" content="(.*?)"/>@si',$lokasyon,$yazi_lokasyon_lat);
     preg_match_all('@<meta itemprop="longitude" content="(.*?)" />@si',$lokasyon,$yazi_lokasyon_lon);
     preg_match_all('@<span id="ctl00_cphContent_ctlRealtyDetailNew1_lblRealtyPrice" class="fz22 ff-m700">(.*?)</span>@si',$lokasyon, $fiyati);
+    preg_match_all('@<div id="realtyInfo" class="detail-panel">(.*?)</div>@si',$lokasyon, $detaylar);
+    preg_match_all('@<ul class="info phone" id="ulPhone">(.*?)</ul>@si',$lokasyon, $contact);
+    preg_match_all('@<meta itemprop="image" content="(.*?)" />@si',$lokasyon, $resimler);
+
 
     echo "latitude = ".$yazi_lokasyon_lat[1][0]."<br>";
     echo "longitude = ".$yazi_lokasyon_lon[1][0]."<br>";
     echo "Fiyatı = ".$fiyati[1][0]."<br>";
+    //echo "Detaylar = ".$detaylar[1][0]."<br>";
+    echo "İletişim Bİlgileri = ".$contact[1][0]."<br>";
+    echo 'Resimler =<img src="'.$resimler[1][0].'"/><br>';
 
 
 
